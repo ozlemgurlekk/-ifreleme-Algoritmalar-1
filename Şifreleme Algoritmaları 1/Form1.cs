d550daad36b96c18cc95c8e7a82c8cd027b6caf4
ÖZLEM <126457800+ozlemgurlekk@users.noreply.github.com>
2024-02-18 22:30:04 +0300

    Proje dosyası ekle.

---
+++ Şifreleme Algoritmaları 1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Şifreleme_Algoritmaları_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string anametin = textBox1.Text;
+            byte[] veridizim = ASCIIEncoding.ASCII.GetBytes(anametin);
+            string sifrelimetin = Convert.ToBase64String(veridizim);
+            textBox2.Text = sifrelimetin;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string metincoz = textBox2.Text;
+            byte[] veridizimcozulen = Convert.FromBase64String(metincoz);
+            string anametin2 = ASCIIEncoding.ASCII.GetString(veridizimcozulen);
+            textBox1.Text = anametin2;
+        }
+    }
+}
---
+++ Şifreleme Algoritmaları 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Şifreleme Algoritmaları 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Şifreleme Algoritmaları 1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("6cdfa931-5b3a-4d50-8e88-6373c685dec5")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
